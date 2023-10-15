3b4c653f5dcab83b4ee0d9e130e04c366056791d
SirLich <lrkoehler77@gmail.com>
2023-10-15 17:10:21 +0200

    Add skeleton for resource loading

--- addons/generic_graph/editor/node_selection.gd
+++ addons/generic_graph/editor/node_selection.gd
@@ -7,15 +7,26 @@ extends MarginContainer
 func _enter_tree() -> void:
 	print("CHILD ENTER TREE")
 
+func _build_tree(root):
+	var out_resources = []
+	var dir = DirAccess.open("res://")
+	dir.list_dir_begin()
+	var file_name = dir.get_next()
+	while file_name != "":
+		var resource = ResourceLoader.load(file_name)
+		if resource != null and resource is GenericGraphNode:
+			var child = root.create_item(root)
+			child.set_text(0, file_name)
+			out_resources.append(resource)
+		file_name = dir.get_next()
+	
+
+	
 func _ready():
 	print("OK")
 	var root = tree.create_item()
 	tree.hide_root = true
-	var child1 = tree.create_item(root)
-	var child2 = tree.create_item(root)
-	var subchild1 = tree.create_item(child1)
-	subchild1.set_text(0, "Subchild1")
-
+	_build_tree(root)
 
 func _on_tree_item_activated() -> void:
 	pass # Replace with function body.
