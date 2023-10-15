2373e4eaafd9b80ae8b1dbbbe4b9c32904d7362f
SirLich <lrkoehler77@gmail.com>
2023-10-15 20:58:42 +0200

    Add some assurances that a graph exists before editing

--- addons/generic_graph/editor/generic_graph_editor.gd
+++ addons/generic_graph/editor/generic_graph_editor.gd
@@ -2,18 +2,24 @@
 extends Control
 class_name GenericGraphEditor
 
-@export var step_selection_prototype : PackedScene
-@onready var title: Label = $MarginContainer/GraphEdit/Title
-@onready var graph_edit: GraphEdit = $MarginContainer/GraphEdit
-@onready var margin_container: MarginContainer = $MarginContainer
+@export var selector_prototype : PackedScene
+@onready var title: Label = $GraphEditContainer/GraphEdit/Title
+@onready var graph_edit: GraphEdit = %GraphEdit
+@onready var no_graph_label: Label = $GraphEditContainer/NoGraphLabel
 
-var step_selection : GenericGraphNodeSelection
+# The selection widget, if active
+var selector : GenericGraphNodeSelection
+# The Graph that's currently being edited.
+var generic_graph : GenericGraph
 
-func start_editing_asset(step_graph : GenericGraph):
-	title.text = step_graph.name
 
-func is_left_click(event):
-	return event is InputEventMouseButton and event.button_index == 1
+func _ready() -> void:
+	evaluate_rendering()
+	
+func start_editing_asset(generic_graph_ : GenericGraph):
+	generic_graph = generic_graph_
+	title.text = generic_graph.name
+	evaluate_rendering()
 	
 func _on_graph_edit_popup_request(position: Vector2) -> void:
 	spawn_selector()
@@ -23,15 +29,31 @@ func on_node_created(graph_node):
 	
 func spawn_selector():
 	despawn_selector()
-	step_selection = step_selection_prototype.instantiate()
-	self.add_child(step_selection)
-	step_selection.global_position = get_global_mouse_position()
-	step_selection.on_node_created.connect(on_node_created)
+	selector = selector_prototype.instantiate()
+	self.add_child(selector)
+	selector.global_position = get_global_mouse_position()
+	selector.on_node_created.connect(on_node_created)
 	
 func despawn_selector():
-	if step_selection != null:
-		step_selection.queue_free()
+	if selector != null:
+		selector.queue_free()
 	
 func _on_graph_edit_gui_input(event: InputEvent) -> void:
-	if is_left_click(event):
+	if _is_left_click(event):
 		despawn_selector()
+
+func evaluate_rendering():
+	if is_graph_valid():
+		print("Is Valid")
+		no_graph_label.visible = false
+		graph_edit.mouse_filter = Control.MOUSE_FILTER_STOP
+	else:
+		print("Is Not Valid")
+		no_graph_label.visible = true
+		graph_edit.mouse_filter = Control.MOUSE_FILTER_IGNORE
+		
+func is_graph_valid():
+	return generic_graph != null
+	
+func _is_left_click(event):
+	return event is InputEventMouseButton and event.button_index == 1
