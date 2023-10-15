558419cbfd9c8336a2175e2c101f25e918d518ed
SirLich <lrkoehler77@gmail.com>
2023-10-15 17:41:40 +0200

    Finish resource loading logic

--- addons/generic_graph/editor/node_selection.gd
+++ addons/generic_graph/editor/node_selection.gd
@@ -6,19 +6,30 @@ extends MarginContainer
 
 func _enter_tree() -> void:
 	print("CHILD ENTER TREE")
-
-func _build_tree(root):
-	var out_resources = []
-	var dir = DirAccess.open("res://")
-	dir.list_dir_begin()
-	var file_name = dir.get_next()
-	while file_name != "":
-		var resource = ResourceLoader.load(file_name)
-		if resource != null and resource is GenericGraphNode:
-			var child = root.create_item(root)
-			child.set_text(0, file_name)
-			out_resources.append(resource)
-		file_name = dir.get_next()
+	
+func _get_file_names_recursive(path) -> PackedStringArray:
+	var out = []
+	var dir = DirAccess.open(path)
+	if not dir:
+		return []
+		
+	for dir_name in dir.get_directories():
+		var dir_path = path +  "/" + dir_name
+		out.append_array(_get_file_names_recursive(dir_path))
+	
+	for file_name in dir.get_files():
+		var file_path = path + "/" + file_name
+		out.append(file_path)
+	return out
+	
+func _build_tree(tree : Tree):
+	for path in _get_file_names_recursive("res://"):
+		if ResourceLoader.exists(path):
+			var resource = ResourceLoader.load(path)
+			if resource is GenericGraphNode:
+				print("WOW")
+				var child = tree.create_item()
+				child.set_text(0, path.get_file())
 	
 
 	
@@ -26,7 +37,8 @@ func _ready():
 	print("OK")
 	var root = tree.create_item()
 	tree.hide_root = true
-	_build_tree(root)
+	_build_tree(tree)
+	
 
 func _on_tree_item_activated() -> void:
 	pass # Replace with function body.
